89a775823e6bf6ddfdaa1bf99d06d57355f5e1be
Pavel Kadantsev <p.a.kadantsev@gmail.com>
2018-06-30 23:25:18 +0300

    Removed verbose args

--- password_checker.py
+++ password_checker.py
@@ -6,17 +6,13 @@ from os.path import exists
 from re import findall
 
 
-def create_args_parser(url):
+def create_args_parser():
     parser = argparse.ArgumentParser()
-    parser.add_argument('-p', '--password', required = True)
-    parser.add_argument('-w', '--weblist', default = url)
     parser.add_argument('-u', '--userlist')
     return parser
 
 
 def check_args(parser, args):
-    if args.weblist and args.userlist:
-        parser.error('Arguments conflict, use one source.')
     if args.userlist is not None and not exists(args.userlist):
         parser.error('File not found.')
 
@@ -35,10 +31,8 @@ def load_user_blacklist(filepath):
         return blacklist_data.read().split('\n')
 
 
-def is_password_in_blacklist(password, 
-                             web_blacklist,
-                             user_blacklist):
-    return (password in web_blacklist or password in user_blacklist)
+def is_password_in_blacklist(password, blacklist):
+    return password in blacklist
 
 
 def store_regex():
@@ -51,13 +45,14 @@ def store_regex():
               r'[0-9]',  # Contains digits 
               r'[!,?,@,#,$,%,^,&,*,-,_]',  # Contains any of listed special symbols
               r'^(?!\S*(\S)\1{2,})',  # Doesn't contain symbols repeated more than 2 times in succession
-              r'^(?!([8-9]{1}[0-9]{9}))'  #Doesn't contain phone number
+              r'^(?!([8-9]{1}[0-9]{9}))',  #Doesn't contain phone number
+              r'^(?!.*(1|2)(\d)(\d)(\d)).*'  #Doesn't contain a
               ]
     return regexs
 
 
 def rate_password_strength(user_password, regexs_list):
-    rating = 0
+    rating = 1
     for template in regexs_list:
         if findall(template, user_password):
             print(template)
@@ -66,18 +61,18 @@ def rate_password_strength(user_password, regexs_list):
 
 
 if __name__ == '__main__':
-    url = "https://raw.githubusercontent.com/skyzyx/bad-passwords/master/raw-mutated.txt"
-    parser = create_args_parser(url)
+    URL = "https://raw.githubusercontent.com/skyzyx/bad-passwords/master/raw-mutated.txt"
+    parser = create_args_parser()
     args = parser.parse_args()
     print(args)
     check_args(parser, args)
-    # web_blacklist = fetch_web_blacklist(args.weblist)
-    # user_blacklist = load_user_blacklist(args.userlist)
-    # if is_password_in_blacklist(user_password,
-    #                             web_blacklist,
-    #                             user_blacklist):
-    #      print('Extremely weak password that can be compromised. Try again.\n')
-    #      exit()
-    # else:
-    #     regexs_list = store_regex()
-    #     print(rate_password_strength(user_password, regexs_list))
+    user_password = input('\nType password:')
+    if args.userlist is None:
+        blacklist = fetch_web_blacklist(URL)
+    else:
+        blacklist = load_user_blacklist(args.userlist)
+    if is_password_in_blacklist(user_password, blacklist):
+         print('\nExtremely weak password. It can be compromised. Try again')
+    else:
+        regexs_list = store_regex()
+        print('Rating',rate_password_strength(user_password, regexs_list))
